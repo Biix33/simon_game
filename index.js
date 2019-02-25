a949d10b3f21ea649a9be72b8d682c9fc29f2128
Yoann Bienvenu <ybienvenu@mail.welcome-int.xyz>
2019-02-25 11:56:22 +0100

    module refactoring

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM Ready');
-
-    const colors = ['#f00', '#0f0', '#ff0', '#00f'];
-    const $screen = document.querySelector('#screen');
-
-    let i = 0;
-    const interval = setInterval(function () {
-        const color = colors[i];
-
-        if (!color) {
-            clearInterval(interval);
-            return;
-        }
-
-        $screen.style.backgroundColor = color;
-        i++;
-    }, 1000);
+    const $screen = document.querySelector('#screen');    
     
-    
+    new Simon($screen);
 });
---
+++ simon.js
@@ -0,0 +1,20 @@
+function Simon($screen) {
+    const colors = ['#f00', '#0f0', '#ff0', '#00f'];
+
+    function run() {
+        let i = 0;
+        const interval = setInterval(function () {
+            const color = colors[i];
+
+            if (!color) {
+                clearInterval(interval);
+                return;
+            }
+
+            $screen.style.backgroundColor = color;
+            i++;
+        }, 1000);
+    };
+    // Run the game
+    run();
+};
