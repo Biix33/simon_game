ea5b757d88d3781527f4bf191a840f84ecce3634
Yoann Bienvenu <ybienvenu@mail.welcome-int.xyz>
2019-02-25 11:36:29 +0100

    display colors with interval

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM Ready');
+
+    const colors = ['#f00', '#0f0', '#ff0', '#00f'];
+    const $screen = document.querySelector('#screen');
+
+    let i = 0;
+    const interval = setInterval(function () {
+        const color = colors[i];
+
+        if (!color) {
+            clearInterval(interval);
+            return;
+        }
+
+        $screen.style.backgroundColor = color;
+        i++;
+    }, 1000);
+    
+    
+});
